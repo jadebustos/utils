3580229314b7d78913012d2b3845dd30013a9b01
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2023-09-18 18:15:59 +0200

    widgets example

---
+++ jupyter-gui/modules/input_data.py
@@ -0,0 +1,36 @@
+import ipywidgets as widgets
+from ipyfilechooser import FileChooser
+import os
+
+# widget layout can be modified
+layout = widgets.Layout(width='auto', height='40px') 
+
+# upload a file
+report_file = widgets.FileUpload(
+    accept='*.txt',  # Accepted file extension e.g. '.txt'
+    multiple=True  # True to accept multiple files upload else False
+)
+
+# Create new FileChooser:
+# Path: current directory
+# File: test.txt
+# Title: <b>FileChooser example</b>
+# Show hidden files: no
+# Use the default path and filename as selection: yes
+# Only show folders: no
+fdialog = FileChooser(
+    os.getcwd(),
+    filename='test.txt',
+    title='<b>FileChooser example</b>',
+    show_hidden=False,
+    select_default=True,
+    show_only_dirs=False
+)
+
+dropdown = widgets.Dropdown(
+    options=[('PCA', 1), ('LDA', 2), ('Random Forest', 3)],
+    value=2,
+    description='Dimensionality reduction algorithm:',
+    layout = layout,
+    style= {'description_width': 'initial'}
+)
